875c412b9eb271e8887aa22bd81ac78087d1d052
claudedjoumessi <djoumessiclaude95@gmail.com>
2025-12-19 23:56:25 +0100

    (add): subroutine show_tasks and show_menu

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-task = input("Enter your task: ")
+# task = input("Enter your task: ")
 
-fs = open("tasks.txt", '+a')
-fs.write(task + '\n')
+# fs = open("tasks.txt", '+a')
+# fs.write(task + '\n')
 
-fs.close()
+# fs.close()
+
+def app():
+    # show_menu()
+    show_tasks()
+
+# Subroutine
+# To read all tasks
+def show_tasks():
+    fs = open("tasks.txt")
+    data = fs.read()
+    fs.close()
+    
+    print(data)
+
+# Renders a menu 
+def show_menu():
+    actions = [
+        "Add a task",
+        "Read all tasks",
+        "Modify a task",
+        "Quit"
+    ]
+    ac = 0 # Init an action count
+    for action in actions:
+        ac += 1
+        print(f"{ac}. {action}")
+        
+# Execute app
+app()
